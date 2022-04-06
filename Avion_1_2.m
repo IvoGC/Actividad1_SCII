9a2bd678642206fa9ffd0e2e5009359816851d66
IvoGC <102243324+IvoGC@users.noreply.github.com>
2022-04-06 11:56:36 -0300

    Add files via upload

--- Avion_1_2.m
+++ Avion_1_2.m
@@ -1,56 +1,52 @@
 close all; clear all ; clc;
-% Caso 1. Sistema lineal de cuatro variables de estado
-% El sistema linealizado  en el punto de trabajo [0,0,0,0] obtenemos el
-% siguiente sistema
+%Caso de estudio 3. Sistema lineal de cuatro variables de estado
+%{
+--------------------------------------------------------------------------
+1- Obtener el sistema lineal en variables de estado para el equilibrio X=[0,0,0,0]
+--------------------------------------------------------------------------
+%}
+w=2; a=0.05; b=5; c=100;
+A = [ -a,a,0,0 ; 0,0,0,0 ; w^2,-w^2,0,0 ; c,0,0,0 ];
+B = [ 0; 0 ; w^2*b ; 0 ];
+C = [ 1,0,0,0 ; 0,0,0,1 ];
+D = 0;
 
+sys = ss(A, B, C, D);
 
+%%
 
-
-%A = [ -a,a,0,0 ; 0,0,0,0 ; w^2,-w^2,0,0 ; c,0,0,0 ];
-%B = [ 0; 0 ; w^2*b ; 0 ];
-%C = [ 1,0,0,0 ; 0,0,0,1 ];
-%D = 0;
-
-%sys = ss(A, B, C, D);
-
-% alfa_p  = a(phi-alfa)
-% phi_pp  = -w^2(phi-alfa-b*U)
-% h_p     = c*alfa
-%equivaent
-% x1_p = a(x2-x1)
-% x3_P = -w^2(x2-x1-b*u)
-% x4_p = c*x1
+%{
+--------------------------------------------------------------------------
+2- Obtener la solución numérica del sistema lineal para evaluar cuantitativamente el 
+comportamiento con intención de verificar el correcto planteo. Para hacerlo, se le asignan 
+los valores siguientes a los parámetros, son w=2; a=0,05; b=5; c=100 m/s, (es decir, 
+360Km/h), dt=1e-3
+; y el tiempo de simulación ts = 5 segundos.
+--------------------------------------------------------------------------
+%}
 
 dt = 1e-3;  % step size
-w=2;
-a=0.05;
-b=5;
-c=50;
+w=2; a=0.05; b=5; c=100; t_f=5; %tiempo total de simulacion
+t = 0:dt:(t_f-dt);
 
-t_0=0;
-t_f=1; %tiempo total de simulacion
-
-t = t_0:dt:(t_f-dt);
+%para el punto de operacion tenemos
+alfa =0; phi =0; phi_p =0; h =0;
 
 y =zeros(t_f/dt,4);
+y(1,1) = alfa;  %x1
+y(1,2) = phi;   %x2
+y(1,3) = phi_p; %x3
+y(1,4) = h;     %x4
 
-%alfa(0)=0;
-%phi(0)=0;
-%phi_p(0)=0;
-%h(0)=0;
-
-y(1,1) = 0; %alfa
-y(1,2) = 0; %phi
-y(1,3) = 0; %phi_p
-y(1,4) = 0; %h
-u = ones(t_f/dt,1);
-u = u*0.01;
 
 n = t_f/dt;
 
-
+%definimos una entrada que valga 0.01 durante la mitad de tiempo de
+%simulacion y luego cambie su valor a -0.01
+u = ones(t_f/dt,1);
+u = u*0.01; 
 for j=(n/2) :1: n
-    u(j)=-0.01;
+    u(j)=0;
 end
 
 
@@ -67,6 +63,58 @@ for i=1 : 1 :(n-1)
     y(i+1 , 4) = y(i,4) + dt *c*h_p;
 end
 
-h_0=0; %h inicial 
-plot (t, y(:,4)+h_0)
+h_0=4000; 
+figure
+plot (t, y(:,4)+h_0); title('entrada u(t), ts=5 , h_0=4000[m] y c=100[m/s](360[Km/h])'); xlabel('tiempo[s]'); ylabel('altura h[m]')
+
+
+%%
+
+%{
+--------------------------------------------------------------------------
+Obtener la solución numérica del sistema lineal para c=50 m/s, (es decir, 180Km/h),
+dt=1e-3
+; y el tiempo de simulación de 20 segundos
+--------------------------------------------------------------------------
+%}
+
+dt = 1e-3;  % step size
+w=2; a=0.05; b=5; c=50; t_f=20; %tiempo total de simulacion
+t = 0:dt:(t_f-dt);
+
+%para el punto de operacion tenemos
+alfa =0; phi =0; phi_p =0; h =0;
+
+y =zeros(t_f/dt,4);
+y(1,1) = alfa;  %x1
+y(1,2) = phi;   %x2
+y(1,3) = phi_p; %x3
+y(1,4) = h;     %x4
+n = t_f/dt;
+
+%definimos una entrada que valga 0.01 durante la mitad de tiempo de
+%simulacion y luego cambie su valor a -0.01
+u = ones(t_f/dt,1);
+u = u*0.01; 
+for j=(n/2) :1: n
+    u(j)=0;
+end
+
+
+for i=1 : 1 :(n-1)
+    alfa_p = a*(y(i,2)-y(i,1));
+    phi_p  = y(i,3);
+    phi_pp = (-w^2)*(y(i,2)-y(i,1)-b*u(i));
+    h_p    = c*y(i,1);
+
+    y(i+1 , 1) = y(i,1) + dt *alfa_p;
+    y(i+1 , 2) = y(i,2) + dt *phi_p;
+    y(i+1 , 3) = y(i,3) + dt *phi_pp;
+    y(i+1 , 4) = y(i,4) + dt *c*h_p;
+end
+
+h_0=4000; 
+figure
+plot (t, y(:,4)+h_0); title('entrada u(t), ts=20 , h_0=4000[m] y c=50[m/s](180[Km/h])'); xlabel('tiempo[s]'); ylabel('altura h[m]')
+
 
---
+++ pendulo_invertido.m
@@ -0,0 +1,366 @@
+close all; clear  ; clc;
+%Caso de estudio 4. Sistema no lineal de cuatro variables de estado
+%{ 
+--------------------------------------------------------------------------
+1- Obtener simulaciones del sistema en las condiciones iniciales 
+Xo=[0, 0, -0.01, 0] y Xo=[0, 0, 3.01, 0]
+con dt=10^-4, tiempo de simulacion ts=10 y u=0 
+--------------------------------------------------------------------------
+%}
+
+%parametros para la primera consigna puntos de op X1=[0,0,-0.01,0] y X2=[0,0,3.01,0]
+m = 0.1; F = 0.1; l = 0.6; g = 9.8; M = 0.5; 
+dt = 0.1e-3; 
+tf = 10; %tiempo final de simulacion
+u=0;
+
+%defino condifiones iniciales  a partir del punto de operacion
+delta = 0; delta_p = 0; phi = -0.01; phi_p = 0;
+
+t=0:dt:tf-dt;
+y = zeros(4,tf/dt) ;
+y(1,1) = delta;   % = x1
+y(2,1) = delta_p; % = x2
+y(3,1) = phi;     % = x3
+y(4,1) = phi_p;   % = x4
+
+n = tf/dt;
+
+%Bucle para el sistema NO linealizado
+for i=1 :1:n-1 
+  delta_pp = (-m*g*cos(y(3,i))*sin(y(3,i))+(m*l*(y(4,i))^2*sin(y(3,i)))-(F*y(2,i))+u)/(M+m-m*cos(y(3,i))*cos(y(3,i)));
+  phi_pp   = ((g*sin(y(3,i)))-(delta_pp*cos(y(3,i))))/l;
+  
+  y(1,i+1) = y(1,i) + dt* y(2,i);  
+  y(2,i+1) = y(2,i) + dt* delta_pp;  
+  y(3,i+1) = y(3,i) + dt* y(4,i);  
+  y(4,i+1) = y(4,i) + dt* phi_pp ;
+  
+end
+figure
+plot (t , y(1,:)) ; title('distancia delta con phi_in=-0.01 y m=0.1 sistema_NL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+figure
+plot (t, y(3,:)); title('angulo phi_in=-0.01 y m=0.1 sistema_NL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+
+%defino condifiones iniciales  a partir del punto de operacion
+delta = 0; delta_p = 0; phi = 3.01; phi_p = 0;
+
+t=0:dt:tf-dt;
+y = zeros(4,tf/dt) ;
+y(1,1) = delta;   % = x1
+y(2,1) = delta_p; % = x2
+y(3,1) = phi;     % = x3
+y(4,1) = phi_p;   % = x4
+
+n = tf/dt;
+
+
+%Bucle para el sistema NO linealizado
+for i=1 :1:n-1 
+  delta_pp = (-m*g*cos(y(3,i))*sin(y(3,i))+(m*l*(y(4,i))^2*sin(y(3,i)))-(F*y(2,i))+u)/(M+m-m*cos(y(3,i))*cos(y(3,i)));
+  phi_pp   = ((g*sin(y(3,i)))-(delta_pp*cos(y(3,i))))/l;
+  
+  y(1,i+1) = y(1,i) + dt* y(2,i);  
+  y(2,i+1) = y(2,i) + dt* delta_pp;  
+  y(3,i+1) = y(3,i) + dt* y(4,i);  
+  y(4,i+1) = y(4,i) + dt* phi_pp ;
+  
+end
+figure
+plot (t , y(1,:)) ; title('distancia delta con phi_in=3.01 y m=0.1 sistema_NL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+figure
+plot (t, y(3,:)); title('angulo phi_in=3.01 y m=0.1 sistema_NL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+
+
+
+
+%%
+%{
+--------------------------------------------------------------------------
+2- Modificar la masa m al doble y repetir la operación anterior
+--------------------------------------------------------------------------
+%}
+%parametros para la primera consigna puntos de op X1=[0,0,-0.01,0] y X2=[0,0,3.01,0]
+m = 0.2; F = 0.1; l = 0.6; g = 9.8; M = 0.5; 
+u=0; dt = 0.1e-3; tf = 10; %tiempo final de simulacion
+
+
+%defino condifiones iniciales  a partir del punto de operacion
+delta = 0; delta_p = 0; phi = -0.01; phi_p = 0;
+
+t=0:dt:tf-dt;
+y = zeros(4,tf/dt) ;
+y(1,1) = delta;   % = x1
+y(2,1) = delta_p; % = x2
+y(3,1) = phi;     % = x3
+y(4,1) = phi_p;   % = x4
+
+n = tf/dt;
+
+%Bucle para el sistema NO linealizado
+for i=1 :1:n-1 
+  delta_pp = (-m*g*cos(y(3,i))*sin(y(3,i))+(m*l*(y(4,i))^2*sin(y(3,i)))-(F*y(2,i))+u)/(M+m-m*cos(y(3,i))*cos(y(3,i)));
+  phi_pp   = ((g*sin(y(3,i)))-(delta_pp*cos(y(3,i))))/l;
+  
+  y(1,i+1) = y(1,i) + dt* y(2,i);  
+  y(2,i+1) = y(2,i) + dt* delta_pp;  
+  y(3,i+1) = y(3,i) + dt* y(4,i);  
+  y(4,i+1) = y(4,i) + dt* phi_pp ;
+  
+end
+figure
+plot (t , y(1,:)) ; title('distancia delta con phi_in=-0.01 y m=0.2 sistema_NL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+figure
+plot (t, y(3,:)); title('angulo phi_in=-0.01 y m=0.2 sistema_NL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+
+%defino condifiones iniciales  a partir del punto de operacion
+delta = 0; delta_p = 0; phi = 3.01; phi_p = 0;
+
+t=0:dt:tf-dt;
+y = zeros(4,tf/dt) ;
+y(1,1) = delta;   % = x1
+y(2,1) = delta_p; % = x2
+y(3,1) = phi;     % = x3
+y(4,1) = phi_p;   % = x4
+
+n = tf/dt;
+%Bucle para el sistema NO linealizado
+for i=1 :1:n-1 
+  delta_pp = (-m*g*cos(y(3,i))*sin(y(3,i))+(m*l*(y(4,i))^2*sin(y(3,i)))-(F*y(2,i))+u)/(M+m-m*cos(y(3,i))*cos(y(3,i)));
+  phi_pp   = ((g*sin(y(3,i)))-(delta_pp*cos(y(3,i))))/l;
+  
+  y(1,i+1) = y(1,i) + dt* y(2,i);  
+  y(2,i+1) = y(2,i) + dt* delta_pp;  
+  y(3,i+1) = y(3,i) + dt* y(4,i);  
+  y(4,i+1) = y(4,i) + dt* phi_pp ;
+  
+end
+figure
+plot (t , y(1,:)) ; title('distancia delta con phi_in=3.01 y m=0.2 sistema_NL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+figure
+plot (t, y(3,:)); title('angulo phi_in=3.01 y m=0.1 sistema_NL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+
+
+%%
+
+%{
+--------------------------------------------------------------------------
+3- Obtener la representación lineal en variables de estado para el
+equilibrio inestable X=[0,0,0,0]
+--------------------------------------------------------------------------
+%}
+m = 0.1; F = 0.1; l = 0.6; g = 9.8; M = 0.5; 
+u=0; tf=5; dt=0.1e-3;
+t= 0:dt:tf;
+
+A = [0,1,0,0 ; 0,(-F/M),(-m/M),0 ; 0,0,0,1 ; 0,(F/(l*M)),(((M+m)*g)/(l*M)),0 ];
+B = [0 ; (1/M) ; (-1/(l*M)) ; 0];
+C = [1,0,0,0 ; 0,0,1,0];
+D = 0;
+
+sys=ss(A,B,C,D);
+%lsim(sys,u,t);
+
+%%
+
+
+
+
+
+
+
+%%
+close all; clear  ; clc;
+%{
+--------------------------------------------------------------------------
+4- Obtener la solución numérica de los dos sistemas, del lineal y del no lineal para evaluar 
+cuantitativamente la equivalencia, modificando m de 0,1 a 0,01 y la longitud l a 1,2m.
+--------------------------------------------------------------------------
+%}
+m = 0.01; F = 0.1; l = 1.2; g = 9.8; M = 0.5; 
+u=0; tf=2; dt=0.1e-3;
+
+%defino condifiones iniciales  a partir del punto de operacion
+delta = 0; delta_p = 0; phi = -0.01; phi_p = 0;
+
+t=0:dt:tf-dt;
+%Sistema NO Linealizado
+yNL = zeros(4,tf/dt) ;
+yNL(1,1) = delta;   % = x1
+yNL(2,1) = delta_p; % = x2
+yNL(3,1) = phi;     % = x3
+yNL(4,1) = phi_p;   % = x4
+
+
+n = tf/dt;
+
+%Bucle para el sistema NO Linealizado
+for i=1 :1:n-1 
+  delta_pp = (-m*g*cos(yNL(3,i))*sin(yNL(3,i))+(m*l*(yNL(4,i))^2*sin(yNL(3,i)))-(F*yNL(2,i))+u)/(M+m-m*cos(yNL(3,i))*cos(yNL(3,i)));
+  phi_pp   = ((g*sin(yNL(3,i)))-(delta_pp*cos(yNL(3,i))))/l;
+  
+  yNL(1,i+1) = yNL(1,i) + dt* yNL(2,i);  
+  yNL(2,i+1) = yNL(2,i) + dt* delta_pp;  
+  yNL(3,i+1) = yNL(3,i) + dt* yNL(4,i);  
+  yNL(4,i+1) = yNL(4,i) + dt* phi_pp ;
+  
+end
+
+%Sistema Linealizado
+y = zeros(4,tf/dt) ;
+y(1,1) = delta;   % = x1
+y(2,1) = delta_p; % = x2
+y(3,1) = phi;     % = x3
+y(4,1) = phi_p;   % = x4
+
+% % %Bucle para el sistema Linealizado en X=[0,0,0,0]
+for i=1 :1:n-1 
+  y(1,i+1) = y(1,i) + dt* y(2,i);
+    
+  y(2,i+1) = y(2,i) + dt* (-((m*g*y(3,i))/M)+(((m*l*(y(4,i))^2*y(3,i))/M))-(((F*y(2,i))/M))+(u/M));
+    
+  y(3,i+1) = y(3,i) + dt* y(4,i);
+    
+  y(4,i+1) = y(4,i) + dt* ((y(3,i)*g*(m+M))/(M*l)-(((m*y(4,i))^2*y(3,i))/M)+((F*y(2,i))/(M*l))-(u/(M*l))) ;
+    
+end
+
+figure
+plot (t , yNL(1,:),'G') ; 
+title('distancia delta con phi=-0.01, l=1.2 y m=0.01 sistemaNL');
+xlabel('tiempo[s]');
+ylabel('delta[m]');
+hold on
+plot (t , y(1,:),'R') ; title('distancia delta con phi=-0.01, l=1.2 y m=0.01 sistemaL VS sistemaNL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+
+figure
+plot (t, yNL(3,:),'G'); title('angulo phi=-0.01, l=1.2 y m=0.01 sistemaNL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+hold on 
+plot (t, y(3,:),'R'); title('angulo phi=-0.01, l=1.2 y m=0.01 sistemaL VS sistemaNL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+
+%%
+%{
+--------------------------------------------------------------------------
+5- Obtener el sistema lineal para el equilibrio estable X=[0,0,pi,0]
+--------------------------------------------------------------------------    
+%}
+m = 0.1; F = 0.1; l = 0.6; g = 9.8; M = 0.5; 
+u=0; tf=5; dt=0.1e-3;
+t= 0:dt:tf;
+
+A = [0,1,0,0 ; 0,(-F/M),(-m*g/M),0 ; 0,0,0,1 ; 0,(-F/(l*M)),(-((M+m)*g)/(l*M)),0 ];
+B = [0 ; (1/M) ; (-1/(l*M)) ; 0];
+C = [1,0,0,0 ; 0,0,1,0];
+D = 0;
+
+sys=ss(A,B,C,D);
+%lsim(sys,u,t);
+
+
+
+%%
+close all; clear  ; clc;
+%{
+--------------------------------------------------------------------------
+6- Obtener la solución numérica de los dos sistemas, del lineal y del no lineal para evaluar 
+cuantitativamente la equivalencia, modificando m de 0,1 a 0,01 y la longitud l a 1,2m.
+--------------------------------------------------------------------------
+%}
+m = 0.01; F = 0.1; l = 1.2; g = 9.8; M = 0.5; 
+u=0; tf=2; dt=0.1e-3;
+
+%defino condifiones iniciales  a partir del punto de operacion
+delta = 0; delta_p = 0; phi = -3.01; phi_p = 0;
+
+t=0:dt:tf-dt;
+%Sistema NO Linealizado
+yNL = zeros(4,tf/dt) ;
+yNL(1,1) = delta;   % = x1
+yNL(2,1) = delta_p; % = x2
+yNL(3,1) = phi;     % = x3
+yNL(4,1) = phi_p;   % = x4
+
+n = tf/dt;
+
+%Bucle para el sistema NO Linealizado
+for i=1 :1:n-1 
+  delta_pp = (-m*g*cos(yNL(3,i))*sin(yNL(3,i))+(m*l*(yNL(4,i))^2*sin(yNL(3,i)))-(F*yNL(2,i))+u)/(M+m-m*cos(yNL(3,i))*cos(yNL(3,i)));
+  phi_pp   = ((g*sin(yNL(3,i)))-(delta_pp*cos(yNL(3,i))))/l;
+  
+  yNL(1,i+1) = yNL(1,i) + dt* yNL(2,i);  
+  yNL(2,i+1) = yNL(2,i) + dt* delta_pp;  
+  yNL(3,i+1) = yNL(3,i) + dt* yNL(4,i);  
+  yNL(4,i+1) = yNL(4,i) + dt* phi_pp ;
+  
+end
+
+%Sistema Linealizado
+y = zeros(4,tf/dt) ;
+y(1,1) = delta;   % = x1
+y(2,1) = delta_p; % = x2
+y(3,1) = phi;     % = x3
+y(4,1) = phi_p;   % = x4
+
+% % %Bucle para el sistema Linealizado en X=[0,0,pi,0]
+for i=1 :1:n-1 
+    delta_pp =  (u-F*y(2,i)+m*l*(y(4,i))^2*(-y(3,i)+pi)+m*g*(-y(3,i)+pi))/M;
+    phi_pp = (g*(-y(3,i)+pi)+delta_pp)/l ;
+    
+    y(1,i+1) = y(1,i) + dt* y(2,i);
+    y(2,i+1) = y(2,i) + dt* delta_pp;
+    y(3,i+1) = y(3,i) + dt* y(4,i);
+    y(4,i+1) = y(4,i) + dt* phi_pp ;
+end
+
+figure
+plot (t , yNL(1,:),'G') ; title('distancia delta con phi=3.01, l=1.2 y m=0.01 sistemaNL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+hold on
+plot (t , y(1,:),'R') ; title('distancia delta con phi=3.01, l=1.2 y m=0.01 sistemaL VS sistemaNL'); xlabel('tiempo[s]'); ylabel('delta[m]');
+
+figure
+plot (t, yNL(3,:),'G'); title('angulo phi=3.01, l=1.2 y m=0.01 sistemaNL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+hold on 
+plot (t, y(3,:),'R'); title('angulo phi=3.01, l=1.2 y m=0.01 sistemaL VS sistemaNL'); xlabel('tiempo[s]'); ylabel('phi[rad]');
+
+
+
+
+
+
+
+%%
+
+% % % %Bucle para el sistema linealizado en X=[0,0,0,0]
+% % for i=1 :1:n-1 
+% %   y(1,i+1) = y(1,i) + dt* y(2,i);
+% %     
+% %   y(2,i+1) = y(2,i) + dt* (-((m*g*y(3,i))/M)+(((m*l*(y(4,i))^2*y(3,i))/M))-(((F*y(2,i))/M))+(u/M));
+% %     
+% %   y(3,i+1) = y(3,i) + dt* y(4,i);
+% %     
+% %   y(4,i+1) = y(4,i) + dt* ((y(3,i)*g*(m+M))/(M*l)-(((m*y(4,i))^2*y(3,i))/M)+((F*y(2,i))/(M*l))-(u/(M*l))) ;
+% %     
+% % end
+
+
+% % %Bucle para el sistema linealizado
+for i=1 :1:n-1 
+  y(1,i+1) = y(1,i) + dt* y(2,i);
+    
+  y(2,i+1) = y(2,i) + dt* (-((m*g*y(3,i))/M)+(((m*l*(y(4,i))^2*y(3,i))/M))-(((F*y(2,i))/M))+(u/M));
+    
+  y(3,i+1) = y(3,i) + dt* y(4,i);
+    
+  y(4,i+1) = y(4,i) + dt* ((y(3,i)*g*(m+M))/(M*l)-(((m*y(4,i))^2*y(3,i))/M)+((F*y(2,i))/(M*l))-(u/(M*l))) ;
+    
+end
+
+
+
+%A = [0,1,0,0 ; 0,(-F/M),(-m/M),0 ; 0,0,0,1 ; 0,(F/(l*M)),(((M+m)*g)/(l*M))]
+%B = [0 ; (1/M) ; (-1/(l*M)) ; 0]
+%C = [1,0,0,0 ; 0,0,1,0]
+%D = 0
+
+
+
